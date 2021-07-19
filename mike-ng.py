d918210e377fcb125ccf7a84d560db5061185f48
m1ke-adams <87667679+m1ke-adams@users.noreply.github.com>
2021-07-19 21:12:20 +0300

    Update mike-ng.py

--- mike-ng.py
+++ mike-ng.py
@@ -1,7 +1,7 @@
 import os
 
-input ("yapımcı: mike adams, bu yazılım crunch kullanımını kolaylaştırmak için yapılmıştır. 19.07.2021")
-input ("wifi hacking python")
+print("yapımcı: mike adams, bu yazılım crunch kullanımını kolaylaştırmak için yapılmıştır. 19.07.2021")
+print("wifi hacking python")
 sayimin = input("wordlist için minimum uzunluk sayısı: ")
 sayimax = input("wordlist için maximum uzunluk sayısı: ")
 harfler = input("wordlistte ne tür karakterler olacak: ")
